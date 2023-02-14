2a8d48a0795723a31a52a431e9bd8355557f8fd0
LarispPigasse <renato.casalena@gmail.com>
2023-02-14 22:58:37 +0100

    Sidebar - ritocchi

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -3,8 +3,8 @@ import { Outlet } from "react-router-dom";
 
 function Content() {
   return (
-    <div>
-        <div className='mx-auto py-2 px-4'>
+    <div className='flex flex-col w-full'>
+        <div className='mx-auto py-2 px-4 w-full'>
           <Outlet />
         </div>
     </div>
